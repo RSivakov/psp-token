195f76ea9cdb3827ca64c2afdb5d76f4fc5338b5
Jason Raymond Bell <jason@epixxware.com>
2021-12-15 16:28:59 +0000

    feat: Add minter cap to BEP20 PSP

--- contracts/PSP_BEP20.sol
+++ contracts/PSP_BEP20.sol
@@ -5,7 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
 contract PSP_BEP20 is ERC20Capped, ERC20Permit, AccessControlEnumerable {
+    struct Supply {
+        uint256 cap;
+        uint256 total;
+    }
+
+    event MinterCapUpdated(address indexed minter, uint256 cap);
+
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    mapping(address => Supply) public minterSupply;
 
     constructor() ERC20("ParaSwap", "PSP") ERC20Capped(2_000_000_000e18) ERC20Permit("ParaSwap") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -16,16 +25,30 @@ contract PSP_BEP20 is ERC20Capped, ERC20Permit, AccessControlEnumerable {
     }
 
     function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) returns (bool) {
+        Supply storage s = minterSupply[msg.sender];
+        s.total += amount;
+        require(s.total <= s.cap, "minter cap exceeded");
         _mint(to, amount);
         return true;
     }
 
     function burn(address from, uint256 amount) external onlyRole(MINTER_ROLE) returns (bool) {
+        Supply storage s = minterSupply[msg.sender];
+        s.total -= amount;
         _burn(from, amount);
         return true;
     }
 
     function getOwner() external view returns (address) {
         return getRoleMember(DEFAULT_ADMIN_ROLE, 0);
+    }
+
+    function getMinterCap(address minter) external view returns (uint256) {
+        return minterSupply[minter].cap;
+    }
+
+    function setMinterCap(address minter, uint256 cap) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        minterSupply[minter].cap = cap;
+        emit MinterCapUpdated(minter, cap);
     }
 }
